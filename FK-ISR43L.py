2fa98772db3fe27fe742389d19df2b7bb2e4a954
AstroCode <astrocode.info@gmail.com>
2025-03-23 07:53:16 +0600

    Update FK-ISR43L.py

--- FK-ISR43L.py
+++ FK-ISR43L.py
@@ -29,7 +29,7 @@ def print_banner():
 ▐┼▐▒▒██▄▄▄▄██▄▄▄▄██▒▒▒  [bright_yellow][[/][bold red]WARNING[/][bright_yellow]][/] [bold white]Educational Purposes Only[/]
 ▐┼▐▒▒██████████████▒▒▒  [bright_yellow][[/][bold cyan]INFO[/][bright_yellow]][/] [bold white]Press Ctrl+C to Stop Attack[/]
 ▐▄▐████─▀▐▐▀█─█─▌▐██▄▒  [bright_yellow][[/][bold green]MODE[/][bright_yellow]][/] [bold white]Unlimited Threads | Auto-Timer[/]
-▒▒█████──ASTRO──▐███▌
+▒▒█████───ASTRO───▐███▌
 ▒▒█▀▀██▄█─▄───▐─▄███▀▒
 ▒▒█▒▒███████▄██████▒▒▒
 ▒▒▒▒▒██████████████▒▒▒
